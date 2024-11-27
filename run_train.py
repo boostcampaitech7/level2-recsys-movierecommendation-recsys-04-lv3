ab2edd6d4b7eae2f8cdd25b71baa89dc45852f17
yunhyechoi <yunhye2011@gamil.com>
2024-11-27 14:22:16 +0900

    Feat: Add MultiEASE training
    # 48

--- run_train.py
+++ run_train.py
@@ -7,15 +7,18 @@ import torch
 from torch.utils.data import DataLoader
 
 from src.data.dataset import MovieLensDataset
-from src.models.EASE import EASE
+from src.models.EASE import EASE, MultiEASE
 from src.utils.utils import check_path, set_seed, generate_submission_file
+from trainer import EASETrainer, MultiEASETrainer, full_sort_predict
+from logging import getLogger
 
 
 def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--data_dir", default="./data/train/", type=str)
-    parser.add_argument("--output_dir", default="output/", type=str)
+    parser.add_argument("--output_dir", default="./saved/submission/", type=str)
+    parser.add_argument("--model_dir", default="./saved/model/", type=str)
 
     # model args
     parser.add_argument("--model", default="ease", type=str)    
@@ -31,62 +34,93 @@ def main():
     parser.add_argument("--valid_size", default=0.1, type=float)   
     parser.add_argument("--num_workers", default=4, type=int)   
 
-    parser.add_argument(
-        "--weight_decay", type=float, default=0.0, help="weight_decay of adam"
-    )
+    parser.add_argument("--weight_decay", type=float, default=0.0, help="weight_decay of adam")
 
     parser.add_argument("--gpu_id", type=str, default="0", help="gpu_id")
+    parser.add_argument("--early_stopping", type=int, default=10, help="round of early stopping")
+
+    
 
     args = parser.parse_args()
+    logger = getLogger()
 
     set_seed(args.seed)
-    check_path(args.output_dir)
+    check_path(args.model_dir)
 
     os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_id
     args.cuda_condition = torch.cuda.is_available() and args.cuda
+    torch.cuda.empty_cache()
 
     # save model args
     args_str = f"{args.model}"
-    args.log_file = os.path.join(args.output_dir, args_str + ".txt")
+    args.log_file = os.path.join(args.model_dir, args_str + ".txt")
     print(str(args))
 
     # save model
     checkpoint = args_str + ".pt"
-    args.checkpoint_path = os.path.join(args.output_dir, checkpoint)
+    args.checkpoint_path = os.path.join(args.model_dir, checkpoint)
 
+    print('----------------Train test split-------------------')
     train_dataset = MovieLensDataset(args, data_type="train")
+
+    num_items = train_dataset.n_items
+    num_users = train_dataset.n_users
+    user_valid = train_dataset.user_valid
+    
+    batch_size = args.batch_size if args.model != 'ease' else num_users
+    shuffle = True if args.model != 'ease' else False
+
     train_dataloader = DataLoader(
         train_dataset,
-        batch_size = args.batch_size, 
-        shuffle = True,
+        batch_size = batch_size, 
+        shuffle = shuffle,
         pin_memory = True,
         num_workers = args.num_workers,
     ) # ease에서는 안쓰임
 
     valid_dataset = MovieLensDataset(args, data_type="valid")
     valid_dataloader = DataLoader(
-        valid_dataset,
-        batch_size = args.batch_size, 
-        shuffle = True,
+        train_dataset,
+        batch_size = batch_size, 
+        shuffle = False,
         pin_memory = True,
         num_workers = args.num_workers,
     ) # ease에서는 안쓰임
 
+    print('----------------Train model-------------------')
     if args.model == 'ease':
         model = EASE(args)
-        model.fit(train_dataset)
-        ndcg, hit, recall =  model.evaluate(train_dataset)
-        print(f'NDCG@10: {ndcg:.5f}| HIT@10: {hit:.5f}| RECALL@10: {recall:.5f}')
-    
+        trainer = EASETrainer(model, args)
+        args.epochs = 1
     elif args.model == 'multiease':
-        # 구현 예정
-        pass
+        model = MultiEASE(args, num_items).to('cuda')
+        optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+        trainer = MultiEASETrainer(model, optimizer, args)
+        
+    # Training
+    best_recall = 0
+    early_stop = 0
+    for epoch in range(1, args.epochs + 1):
+        train_loss = trainer.train(train_dataloader)
+        ndcg, hit, recall = trainer.evaluate(train_dataloader, user_valid)
 
-    model = EASE(args)
-    model.fit(valid_dataset)
-    rec_pred = model.predict(valid_dataset)
-    submission = generate_submission_file(valid_dataset, rec_pred)
-    submission.to_csv(os.path.join(args.output_dir, args.model +'_submission_'+ pd.Timestamp.now().strftime("%Y-%m-%d %H%M%S") +'.csv'), index=False)
+        print(f'Epoch: {epoch:3d}| Train loss: {train_loss:.5f}| NDCG@10: {ndcg:.5f}| HIT@10: {hit:.5f}| RECALL@10: {recall:.5f}')
+
+        if best_recall < recall:
+            best_recall = recall
+            trainer.save_model(args.checkpoint_path, args.model)
+            early_stop = 0
+        else:
+            early_stop += 1
+            if early_stop == args.early_stopping:
+                print('early stop!')
+                break
+    
+
+    print('----------------submission file-------------------')   
+    pred = full_sort_predict(model, valid_dataloader)
+    submission = generate_submission_file(valid_dataset, pred)
+    submission.to_csv(os.path.join(args.output_dir, args.model +'_submission_'+ pd.Timestamp.now().strftime("%Y-%m-%d %H:%M") +'.csv'), index=False)
 
 if __name__ == "__main__":
     main()
